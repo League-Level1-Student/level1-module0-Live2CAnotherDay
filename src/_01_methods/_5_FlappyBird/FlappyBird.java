41bdd74be44503837973c76ff4c00b50849875e9
league <league@imac-24.attlocal.net>
2022-07-15 18:28:17 -0700

    Finish the flappy bird thingie

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,10 +1,20 @@
 package _01_methods._5_FlappyBird;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class FlappyBird extends PApplet {
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
+    static final int WIDTH = 500;
+    static final int HEIGHT = 750;
+    PImage background;
+    PImage bird;
+    PImage bottomPipe;
+    PImage topPipe;
+    int y = HEIGHT / 2;
+    int birdYVelocity = 0;
+	int gravity = 1;
+	int pipeX = 500;
+	int topPipeY = (int)random(0, );
 
     @Override
     public void settings() {
@@ -13,12 +23,42 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void setup() {
-        
+    	background = loadImage("flappyBackground.jpg");
+    	bird = loadImage("bird.png");
+    	bird.resize(50, 50);
+    	bottomPipe = loadImage("bottomPipe.png");
+    	topPipe = loadImage("topPipe.png");
+    	
     }
 
     @Override
     public void draw() {
-        
+    	
+    	background(background);
+    	fill(255, 255, 0);
+    	stroke(255, 255, 255);
+    	int x = WIDTH / 4;
+    	
+    	
+    	image(bottomPipe, pipeX , pipeY);
+    	pipeX -= 1;
+    	image(topPipe, pipeX, pipeY);
+    	if (pipeX < -50) {
+    		pipeX = 500;
+    	}
+    	
+    	
+    	
+    	if (mousePressed) {
+    		birdYVelocity = -10;
+    		
+    		
+    		 
+    	}
+    	birdYVelocity = birdYVelocity + gravity;
+		y = y + birdYVelocity;
+		image(bird, x, y );	
+
     }
 
     static public void main(String[] args) {
